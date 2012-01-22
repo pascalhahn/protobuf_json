f0f21374a048c00a20ea25463ffc01628650715b
Pascal Hahn <ph@lxd.bz>
2012-01-22 16:27:57 +0100

    initial commit of protobuf_json code

---
+++ protobuf_json.py
@@ -0,0 +1,210 @@
+#!/usr/bin/python
+#
+# Parse json to protobuf messages and vice versa.
+#
+# TODO: add embedded message support from json to proto
+
+
+__author__ = 'Pascal Hahn <ph@lxd.bz>'
+
+
+import simplejson
+
+from google.protobuf.descriptor import FieldDescriptor as protofd
+
+
+class Error(Exception):
+  pass
+
+
+class JsonDataMissingError(Error):
+  pass
+
+
+class ProcessingError(Error):
+  pass
+
+
+class UnsupportedFieldTypeError(Error):
+  pass
+
+
+class ProtoEnumValueNotFoundError(Error):
+  pass
+
+
+TYPE_MAP = {
+    protofd.TYPE_BOOL: bool,
+    protofd.TYPE_FLOAT: float,
+    protofd.TYPE_INT32: int,
+    protofd.TYPE_INT64: int,
+    protofd.TYPE_STRING: unicode,
+    protofd.TYPE_ENUM: int,
+    }
+
+
+def _convert_json_value(field, value, message_class):
+  """Convert the fields value, using the TYPE_MAP.
+
+  Args:
+    field: field instance - proto field we work with
+    value: one of TYPE_MAP.keys() - value to convert
+    message_class: Proto message class
+
+  Returns:
+    value: converted value
+
+  Raises:
+    ProtoEnumValueNotFoundError: if the enum does not have the value
+    UnsupportedFieldTypeError: If the proto field type is not supported
+  """
+  # convert ENUM fields to cleartext for readability of json
+  if field.type == protofd.TYPE_ENUM:
+    if not hasattr(message_class, value):
+      raise ProtoEnumValueNotFoundError(
+        'Enum does not have a value %s' % value)
+    return getattr(message_class, value)
+  # cast fields to their appropriate datatypes according
+  # to TYPE_MAP
+  elif field.type in TYPE_MAP:
+    return TYPE_MAP[field.type](value)
+  else:
+    raise UnsupportedFieldTypeError(
+      'ProtoType %i not supported yet' % field.type)
+
+
+def json2proto(json_str, message_class):
+  """Parse json input to a proto.
+
+  Take a hash in json and parse it to a proto. The resulting protos will
+  always be valid because all the fields of the message are ensured to be
+  valid.
+
+  IsInitialized() is intentionally not called due to the overhead and because
+  it is made sure that protos are valid via code.
+
+  Args:
+    json_str: str - json input
+    message_class: proto Message class - the message that the proto should be
+                   parsed to
+
+  Returns:
+    proto_obj: instance of the message_class: message instance filled with data
+
+  Raises:
+    ProtoEnumValueNotFoundError: if the enum does not have the requested value
+                                 (from _convert_json_value)
+    UnsupportedFieldTypeError: if the proto field type is not supported
+                               (from _convert_json_value)
+    JsonDataMissingError: if the json does not have all the required data
+  """
+  json_data = simplejson.loads(json_str)
+  proto_obj = message_class()
+
+  for field in message_class.DESCRIPTOR.fields:
+    if field.name in json_data:
+      value = json_data[field.name]
+        
+      # treat repeated fields properly, 
+      if field.label == protofd.LABEL_REPEATED:
+        label_obj = getattr(proto_obj, field.name)
+        label_obj.extend([
+            _convert_json_value(field, val, message_class)
+            for val in value])
+      else:
+        setattr(
+          proto_obj, field.name,
+          _convert_json_value(field, value, message_class))
+    # we always want to set a default value explicitly due to weird handling of
+    # required fields with a default
+    # (http://code.google.com/p/protobuf/issues/detail?id=312)
+    elif field.has_default_value:
+      setattr(proto_obj, field.name, field.default_value)
+    else:
+      raise JsonDataMissingError(
+          'Field %s is not set in json data' % field.name)
+
+  return proto_obj
+
+
+def _convert_proto_value(field, value, message_obj):
+  """Convert the fields value, using the TYPE_MAP.
+
+  Args:
+    field: proto field instance - the field we work with
+    value: one of TYPE_MAP.keys() - value to convert
+    message_obj: Proto message instance
+
+  Returns:
+    value: converted value
+
+  Raises:
+    ProtoEnumValueNotFoundError: if the enum does not have the value
+    UnsupportedFieldTypeError: If the proto field type is not supported
+  """
+  # convert ENUM fields to cleartext for readability of json
+  if field.type == protofd.TYPE_ENUM:
+    tmp_field = field.enum_type.values_by_number.get(value, None)
+    if tmp_field == None:
+      raise ProtoEnumValueNotFoundError(
+        'Enum does not have a value %d' % value)
+    return tmp_field.name
+  # no need to cast values from proto, pass them directly if they are supported
+  elif field.type in TYPE_MAP:
+    return value
+  elif field.type == protofd.TYPE_MESSAGE:
+    return _proto2json_data(value)
+  else:
+    raise UnsupportedFieldTypeError(
+      'ProtoType %i not supported yet' % field.type)
+
+
+def proto2json(message_instance):
+  """Parse a proto message instance to json.
+
+  During conversion, also default values are converted. This is so that we can rely on
+  the data to be consistent even across versions of the proto. Otherwise we'd not know
+  if the json data is not a valid dataset if we just had changed in the defaults and
+  if we only added in values with a default.
+
+  Args:
+    message_instance: instance of protobuf message - message to parse
+
+  Returns:
+    json_str: str - message as json string
+
+  Raises:
+    ProtoEnumValueNotFoundError: if the enum does not have the value
+    UnsupportedFieldTypeError: If the proto field type is not supported
+  """
+  return simplejson.dumps(_proto2json_data(message_instance))
+
+
+def _proto2json_data(message_instance):
+  """Parse a proto message instance to a dict.
+
+  During conversion, also default values are converted. This is so that we can rely on
+  the data to be consistent even across versions of the proto. Otherwise we'd not know
+  if the json data is not a valid dataset if we just had changed in the defaults and
+  if we only added in values with a default.
+
+  Args:
+    message_instance: instance of protobuf message - message to parse
+
+  Returns:
+    json_data: dict - message as dict
+
+  Raises:
+    ProtoEnumValueNotFoundError: if the enum does not have the value
+    UnsupportedFieldTypeError: If the proto field type is not supported
+  """
+  json_data = {}
+  for field in message_instance.DESCRIPTOR.fields:
+    value = getattr(message_instance, field.name)
+    if field.label == protofd.LABEL_REPEATED:
+      json_data[field.name] = [_convert_proto_value(field, val, message_instance) for val in value]
+    else:
+      json_data[field.name] = _convert_proto_value(field, value, message_instance)
+  return json_data
+
+# vim: set tabstop=2 softtabstop=2 shiftwidth=2 textwidth=80 syntax=python smarttab expandtab:
---
+++ protobuf_json_test.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python
+#
+# Test the protobuf json library
+
+
+__author__ = 'Pascal Hahn <ph@lxd.bz>'
+
+
+import protobuf_json
+import unittest
+
+from testdata import node_pb2
+from testdata import repeated_field_pb2
+from testdata import embedded_msg_pb2
+
+class Json2ProtoTest(unittest.TestCase):
+  def test_alldataset(self):
+    node_name = 'ber01vmmcbot001.dt.ber01.locn.s.nokia.com'
+    node_state = 'AVAILABLE'
+
+    node = protobuf_json.json2proto(
+        '{"nodeid": "%s", "state": "%s"}' % (node_name, node_state),
+        node_pb2.Node)
+    self.assertEquals(node.nodeid, node_name)
+    self.assertEquals(node.state, node.AVAILABLE)
+    self.assertTrue(node.IsInitialized())
+
+  def test_defaultdata(self):
+    node_name = 'ber01vmmcbot001.dt.ber01.locn.s.nokia.com'
+
+    node = protobuf_json.json2proto(
+        '{"nodeid": "%s"}' % node_name, node_pb2.Node)
+    self.assertEquals(node.nodeid, node_name)
+    self.assertEquals(node.state, node.PLANNED)
+    self.assertTrue(node.IsInitialized())
+
+  def test_raisesJsonDataMissingError(self):
+    self.assertRaises(
+      protobuf_json.JsonDataMissingError,
+      protobuf_json.json2proto, '{}', node_pb2.Node)
+
+  def test_repeated_field(self):
+    msg = protobuf_json.json2proto('{"notes": ["testnote", "testnotes"]}', repeated_field_pb2.TestMessage)
+    self.assertEquals(msg.notes, ['testnote', 'testnotes'])
+
+  def test_raisesProtoEnumValueNotFoundError(self):
+    node_name = 'ber01vmmcbot001.dt.ber01.locn.s.nokia.com'
+    self.assertRaises(
+      protobuf_json.ProtoEnumValueNotFoundError,
+      protobuf_json.json2proto,
+      '{"nodeid": "%s", "state": "%s"}' % (node_name, "WROONG"), node_pb2.Node)
+
+  def test_raisesUnsupportedFieldTypeError(self):
+    self.assertRaises(
+      protobuf_json.UnsupportedFieldTypeError,
+      protobuf_json.json2proto,
+      '{"testmessage": {"test": "test"}}', embedded_msg_pb2.TestMessage)
+
+
+class Proto2JsonTest(unittest.TestCase):
+  def test_complete_node(self):
+    node = node_pb2.Node(nodeid='testnode', state=node_pb2.Node.AVAILABLE)
+    self.assertEquals(protobuf_json.proto2json(node), '{"state": "AVAILABLE", "nodeid": "testnode"}')
+
+  def test_node_defaults(self):
+    node = node_pb2.Node(nodeid='testnode')
+    self.assertEquals(protobuf_json.proto2json(node), '{"state": "PLANNED", "nodeid": "testnode"}')
+
+  def test_repeated_field(self):
+    msg = repeated_field_pb2.TestMessage()
+    msg.notes.extend(['testnote', 'testnote2'])
+    self.assertEquals(protobuf_json.proto2json(msg), '{"notes": ["testnote", "testnote2"]}')
+
+  def test_node_raisesProtoEnumValueNotFoundError(self):
+    node = node_pb2.Node(nodeid='testnode', state=node_pb2.Node.AVAILABLE)
+    node.state = 20
+    self.assertRaises(
+      protobuf_json.ProtoEnumValueNotFoundError,
+      protobuf_json.proto2json,
+      node)
+
+  def test_embedded_msg(self):
+    msg = embedded_msg_pb2.TestMessage()
+    self.assertEquals(protobuf_json.proto2json(msg), '{"testmessage": {"test": "test"}}')
+
+
+if __name__ == '__main__':
+  unittest.main()
+
+# vim: set tabstop=2 softtabstop=2 shiftwidth=2 textwidth=80 syntax=python smarttab expandtab:
